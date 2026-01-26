242a728f5a54688f49ac4ce0299279e3fc8ec952
Nicklas Wallgren <nicklas.wallgren@gmail.com>
2026-01-26 09:11:20 +0100

    chore: remove workarounds for junit4

--- testify-testcontainers/src/main/java/io/github/finoid/testify/testcontainers/TestRule.java
+++
@@ -1,7 +0,0 @@
-package org.junit.rules;
-
-// Temporary fake interface until https://github.com/testcontainers/testcontainers-java/issues/970 has been fixed
-@SuppressWarnings("unused")
-public interface TestRule {
-
-}
